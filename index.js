f2910f73b7d305057014160806a47ce8e6060e59
dov.ji <dov.ji@zoom.us>
2022-08-31 09:45:33 +0800

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = (api, options) => {
   // alias
   const resolve = (path) => api.resolve(path)
   api.chainWebpack(webpackConfig => {
-    config.resolve.webpackConfig
+    webpackConfig.resolve.alias
       .set('api', resolve('./src/api'))
       .set('common', resolve('./src/common'))
       .set('components', resolve('./src/components'))
